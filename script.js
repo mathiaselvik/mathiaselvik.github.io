3e5b25c14070a5f91605bfc9358bd7c14412c887
mathiaselvik <mathias.elvik17@gmail.com>
2026-05-15 15:26:15 +0200

    spill
    lakgt til mini klikker spill

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function handleClick(){
     alert("flink gutt(bad boy)")
 }
+
+
+
+let score=0
+const money=document.getElementById("scoreShow")
+function gi_2(){
+    score=score+2
+    money.textContent = "score: " + score;
+}
